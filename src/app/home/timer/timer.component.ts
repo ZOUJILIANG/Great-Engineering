e1df7ce4ea71652182662d72711520c6ed2603d1
JiLiang Zou <JiLiang.Zou@fil.com>
2019-06-25 16:48:47 +0800

    6-25

--- src/app/home/timer/timer.component.ts
+++ src/app/home/timer/timer.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { TokenAlertService } from 'src/app/service/token-alert.service';
 
 @Component({
   selector: 'app-timer',
@@ -11,7 +13,10 @@ export class TimerComponent implements OnInit {
   public minutes;
   public seconds;
   public oldTime = (new Date('2018/07/27 00:00:00')).getTime(); // 得到毫秒数
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+    private tokenAlertService: TokenAlertService
+  ) { }
 
   ngOnInit() {
     setInterval(() => {
@@ -26,6 +31,15 @@ export class TimerComponent implements OnInit {
       this.minutes = parseInt(((time % (1000 * 60 * 60)) / (1000 * 60)).toString());
       // tslint:disable-next-line:radix
       this.seconds = parseInt(((time % (1000 * 60)) / 1000).toString());
+    });
+  }
+  getHtml() {
+    const asd = this.http.get('http://localhost:8080/wiki/getHtml');
+    asd.subscribe(value => {
+      console.log(value);
+    }, error1 => {
+    // console.log(error1);
+      this.tokenAlertService.alertToken();
     });
   }
 
